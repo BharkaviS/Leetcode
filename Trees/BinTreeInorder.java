7972524c04c2435496544a4678d91dd5e260fb4d
BharkaviS <41267360+BharkaviS@users.noreply.github.com>
2020-05-30 20:04:58 +0530

    Create BinTreeInorder.java

---
+++ Trees/BinTreeInorder.java
@@ -0,0 +1,36 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+*/
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> a = new ArrayList<Integer>();
+        if(root!=null){
+        
+         dfs(root,a);
+        }
+
+        return a;
+    }
+    public void dfs(TreeNode root,List<Integer> a){
+        if(root.left!=null){
+            dfs(root.left,a);
+        }
+        a.add(root.val);
+        if(root.right!=null){
+            dfs(root.right,a);
+        }
+    }
+    
+}
